88294c97660a8b63f9ed035a2d1c89fa3646bba3
inconveniencestore <iivyleaves@gmail.com>
2024-10-02 11:37:14 -0400

    cats r cool

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const header = document.querySelector("h1"); //can also put class name
-console.log(header.parentElement); //should return body if parent element
+const header = document.querySelector("h1"); 
+console.log(header.parentElement); 
 
 //create object to store data
 const DOMSelectors = {
-    header: document.querySelector("h1"),
-    description: document.querySelector(".card-desc"),
-    items: document.querySelectorAll("li"),
+
 };
 
 console.log(DOMSelectors.description);
 
-function changeColor(){
+function createObject(){
     let form = document.querySelector(".enter");
     form.addEventListener("submit", function(event) { //it was click and button
         event.preventDefault();
         console.log(event.target); //event.target = element
         button.style.backgroundColor = "red";
-}) //event happens, run code
+}) 
 }
-//call function
+
 changeColor();
