0535362fa07e66ed1792782117fd4103a7f211e8
inconveniencestore <iivyleaves@gmail.com>
2024-10-10 12:29:39 -0400

    consider: audrey stop bullying me

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const DOMSelectors = {
 
 console.log(DOMSelectors.description);
 
+//event listener for form
 function storeInput(){
 
 }
 
+//get values for the object
+
+//turn stuff into base 16
 function sillyMath(){
 
 }
 
+//create the card and insert?
 function createObject(){
     let form = document.querySelector(".enter");
     form.addEventListener("submit", function(event) { //it was click and button
@@ -24,4 +29,6 @@ function createObject(){
         button.style.backgroundColor = "red";
 }) 
 }
+
+//find and remove buttons, add event listeners?
 
