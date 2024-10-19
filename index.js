45aff46ce6cdadf3540125477659106e2a97fd93
inconveniencestore <iivyleaves@gmail.com>
2024-10-18 23:22:33 -0400

    im going insane. the rats are nibbling at my feet

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ console.log(header.parentElement);  */
 
 //create object to store data
 const DOMSelectors = {
-  colorBox: document.querySelector(`.color-box`),
   button: document.querySelector(`.submit`),
-  remove: document.querySelector(`.remove`),
   name: document.querySelector(`#name`),
   food: document.querySelector(`#food`),
   date: document.querySelector(`#date`),
@@ -13,57 +11,76 @@ const DOMSelectors = {
   box: document.querySelector(".box"),
 };
 
-const colorInfo = [];
-
 function acquireColor(value) {
   let color = 0;
   for (let i = 0; i < value.length; i++) {
     color += value.charCodeAt(i); //combined length, gets unicode for character at the length of the for loop
-    if (color >= 255) {
-      color = color % 256; //remainder
-    }
+    color = color % 256; //keeps it at 255
   }
-  console.log(color);
   return color;
+}
+
+//make text dark if overly light
+function darkText(r, g, b) {
+  console.log(r, g, b);
+  if (r > 190 || g > 190 || b > 190) {
+    console.log("dark");
+    return "0";
+  } else {
+    console.log("light");
+    return "255";
+  }
+}
+
+//clear fields after putting into form
+function clearFields() {
+  DOMSelectors.name.value = "";
+  DOMSelectors.food.value = "";
+  DOMSelectors.date.value = "";
 }
 
 //event listener for form
 DOMSelectors.button.addEventListener("click", function (event) {
   event.preventDefault();
 
-  let name = DOMSelectors.name.value;
-  let food = DOMSelectors.food.value;
-  let date = DOMSelectors.date.value;
+  //make object
+  const info = {
+    name: DOMSelectors.name.value,
+    food: DOMSelectors.food.value,
+    date: DOMSelectors.date.value,
+  };
 
-  colorInfo.push({ name, food, date });
+  //do math
+  let colorOne = acquireColor(info.name);
+  let colorTwo = acquireColor(info.food);
+  let colorThree = acquireColor(info.date);
 
-  console.log(colorInfo);
-
-  let colorOne = acquireColor(name);
-  let colorTwo = acquireColor(food);
-  let colorThree = acquireColor(date);
-  /*   let combinedInput = name + food + date;
-  let colorHash = hashCode(combinedInput);
-  let color = intToRGB(colorHash); // Convert the hash to a hex color code */
+  text = darkText(colorOne, colorTwo, colorThree);
 
   // Corrected the inline style formatting
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
-    `<div class="box" style="background-color: rgb(${colorOne}, ${colorTwo}, ${colorThree});">
-              <ul>
-                  <li>Your Name: ${name} </li>
-                  <li>Favorite Food: ${food} </li>
-                  <li>User's Birth date: ${date} </li>
-              </ul>
-              <div class="color-box" style="background-color: rgb(${colorOne}, ${colorTwo}, ${colorThree});">
-                  <h2>I think you'll like the color:</h2>
-                  <h3>rgb(${colorOne}, ${colorTwo}, ${colorThree})</h3>
-              </div>
-              <button type="remove">remove</button>
+    `<div class="box" style="background-color: rgb(${colorOne}, ${colorTwo}, ${colorThree}); color: rgb(${text}, ${text}, ${text});">
+        <ul>
+            <li>Name: ${info.name} </li>
+            <li>Favorite Food: ${info.food} </li>
+            <li>Birth date: ${info.date} </li>
+        </ul>
+        <div class="color-box" style="background-color: rgb(${colorOne}, ${colorTwo}, ${colorThree});">
+            <h2>I think you'll like the color:</h2>
+            <h3>rgb(${colorOne}, ${colorTwo}, ${colorThree})</h3>
+        </div>
+        <button type="button" class= "remove">remove</button>
       </div>`
   );
-});
 
-/* DOMSelectors.remove.addEventListener("click", function (event) {
-  box.remove;
-}); */
+  const newBox = DOMSelectors.container.lastElementChild; //adds a box to the last child. which should be container
+  const removeButton = newBox.querySelector(".remove"); //selects the button from the container
+
+  removeButton.addEventListener("click", function () {
+    //listens for click then removes
+    newBox.remove();
+  });
+
+  clearFields(info);
+});
